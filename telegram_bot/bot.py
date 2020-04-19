2d2dfb6f0e6ef63ca62aaa4abb4a00b9688abe14
cchadwic <artemkush1@gmail.com>
2020-04-19 14:45:43 +0000

    fix

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -4,6 +4,7 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
 import time
 import logging
 import json
+import os
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-CHOOSING, TYPING_REPLY, PHOTO, FINISH = range(4)
+CHOOSING, TYPING_REPLY, PHOTO, FINISH, WAIT = range(5)
 
 reply_keyboard = [['Info', 'Load photo'],
                   ['Exit']]
@@ -21,18 +22,29 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 markup_start = ReplyKeyboardMarkup(start_keyboard, one_time_keyboard=True)
 
 
-admins_id = [436264579]
+admins_id = [436264579,
+			 ]
+TOKEN = ''
 
 
+def create_file():
+	if os.path.isfile('history.json') is False:
+		d = {}
+		with open('history.json', 'w') as file:
+			json.dump(d, file, indent=4, separators=(',', ': '))
 
 
 def start(update, context):
-    reply_text = "Hi! I can find person in VK."
-    print(update.message.from_user.id)
-    update.message.reply_text(reply_text, reply_markup=markup)
-    #with open('history.json', 'wr') as file:
-        #data = json.load(file)
-    return CHOOSING
+	reply_text = "Hi! I can find person in VK."
+	print(update.message.from_user.id)
+	update.message.reply_text(reply_text, reply_markup=markup)
+	create_file()
+	with open('history.json', 'r') as file:
+		data = json.load(file)
+	with open('history.json', 'w') as file:
+		data[update.message.from_user.id] = {'time': time.time()}
+		json.dump(data, file, indent=4, separators=(',', ': '))
+	return CHOOSING
 
 
 def info(update, context):
@@ -47,6 +59,15 @@ def finish(update, context):
 	reply_text = "Goodbye!"
 	update.message.reply_text(reply_text, markup=markup_start)
 	return ConversationHandler.END
+
+
+def finish_login(update, context):
+	if 'choice' in context.user_data:
+		del context.user_data['choice']
+	reply_text = "Ok!"
+	update.message.reply_text(reply_text, markup=markup_start)
+	return ConversationHandler.END
+
 
 
 def error(update, context):
@@ -84,9 +105,24 @@ def help(update, context):
 		\n/start\n/help")
 
 
+def login_start(update, context):
+	update.message.reply_text("Write password")
+	return WAIT
+
+def login_finish(update, context):
+	if update.message.text == password:
+		update.message.reply_text("Success")
+		if update.message.from_user.id not in admins_id:
+			admins_id.append(update.message.from_user.id)
+		return ConversationHandler.END
+	else:
+		update.message.reply_text("Wrong password")
+		return ConversationHandler.END
+
+
 def main():
     pp = PicklePersistence(filename='conversationbot')
-    updater = Updater('', persistence=pp, use_context=True)
+    updater = Updater(TOKEN, persistence=pp, use_context=True)
     dp = updater.dispatcher
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
@@ -107,9 +143,14 @@ def main():
         name="my_conversation",
         persistent=True
     )
+    conv_handler_login = ConversationHandler(
+        entry_points=[CommandHandler('login', login_start)],
+        states={WAIT: [MessageHandler(Filters.all, login_finish)]},
+        fallbacks=[],)
     dp.add_handler(conv_handler)
+    dp.add_handler(conv_handler_login)
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(MessageHandler(Filters.command, help))
+    #dp.add_handler(MessageHandler(Filters.command, help))
     dp.add_handler(MessageHandler(Filters.all, help))
     
 
